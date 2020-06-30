d84eb9398b3bcd30206e757aace42cf7e5038d18
Gidi Yoffe <gidizor@gmail.com>
2020-06-30 10:45:25 +0200

    *** UPDATE ***
    1. Instead of Baseline + u,v plane rotation angle, the current input are TELESCOPE COORDINATES from which baselines and closure phases are now calculated. The input is a tuple of u,v positions in meters, respectively.
    
    2. Added plots:
    
    * ["closure_phases"]: plots closure phases of all possible 3-telescope combinations
    
    * ["baselines"]: plots telescope positions on the u-v plane and their associated baselines
    
    *** END ***

--- Interferometry Simulator.py
+++ Interferometry Simulator.py
@@ -1,4 +1,5 @@
 from pylab import *
+from itertools import combinations
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 colors = ["maroon", "peru", "olivedrab", "darkseagreen", "lightseagreen", "lightskyblue", "slateblue", "mediumorchid"] # Pallette
@@ -18,9 +19,17 @@ as2deg = mas2deg * 1000 # degrees to arcseconds
 meter2pc = 3.240779289e-17 # meter to parsec
 meter2au = 6.68459e-12 # meter to AU
 
-#### Useful functions ####
+# Angle between two points #
+def angle_between(p1, p2):
 
-# Modulus of a complex number calculator #
+    # p1: tuple (x, y)
+    # p2: tuple (x, y)
+
+    ang1 = np.arctan2(*p1[::-1])
+    ang2 = np.arctan2(*p2[::-1])
+    return np.rad2deg((ang1 - ang2) % (2 * np.pi))
+
+# Modulus of a complex number #
 def complex_modulus(z):
 
     a = z.real
@@ -216,8 +225,7 @@ def plot_u_v_wlSynthesis(Baselines, u_orientation, wavelengths, dec, Hr_angle):
 
             if w == 0:
                 plt.plot(u_proj_u, u_proj_v,
-                         label='B = {0} [m], $\\theta_B$ = {1} [deg]'.format(Baselines[b], round(u_orientation[b], 2)),
-                         color=colors[b])
+                         label='B = {0} [m], $\\theta_B$ = {1} [deg]'.format(Baselines[b], round(u_orientation[b], 2)), color=colors[b])
                 plt.plot(- u_proj_u, - u_proj_v, '.', color=colors[b])
             else:
                 plt.plot(u_proj_u, u_proj_v, '.', color=colors[b])
@@ -226,22 +234,33 @@ def plot_u_v_wlSynthesis(Baselines, u_orientation, wavelengths, dec, Hr_angle):
     plt.legend(fontsize=6)
     plt.show()
 
-def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_knobs):
+def run(wavelengths, beta, detector_coords, Telescopes_coords, source_params, plotting_knobs):
 
-    # wavelengths: Spectral range VECTOR of the interferometer [m]
-    # detector_coords: 1D coords VECTOR of the detector surface [m]
-    # beta: angle of incidence of the beam on the detector [deg]
-    # Baselines: list of tuples of shape: (Baseline length [m], Baseline clockwise rotation with respect to u-v plane [deg])
-    # beta: Angle of incidence of the beam onto the detector [deg]
-    # source_params: DICTIONARY of shape: ["dec"] = source declination [deg], ["lat"] = observatory latitude [deg], ["h"] = VECTOR of Hour Angles of the source, ["dist"] = source distance [pc], ["characteristic_scale"] = characteristic scale of the object's geometry (sigma for Gaussian shapes, outer radius for uniform shapes) [as] \
+    ### wavelengths: Spectral range VECTOR of the interferometer [m]
+    ### detector_coords: 1D coords VECTOR of the detector surface [m]
+    ### beta: angle of incidence of the beam on the detector [deg]
+    ### Telescopes_coords: list of tuples of shape: (Telescope x coordinate [m], Telescope y coordinate [m])
+    ### beta: Angle of incidence of the beam onto the detector [deg]
+    ### source_params: DICTIONARY of shape: ["dec"] = source declination [deg], ["lat"] = observatory latitude [deg], ["h"] = VECTOR of Hour Angles of the source, ["dist"] = source distance [pc], ["characteristic_scale"] = characteristic scale of the object's geometry (sigma for Gaussian shapes, outer radius for uniform shapes) [as] \
     # ["shape"]: STRING of desired shape of object (uniform_ring, uniform_disk, gaussian_ring, gaussian_disk), ["shape_params"]: LIST of parameters of the shape, as required by each function except for centroid position and sigma_grid
-    # plotting_knobs: DICTIONARY of booleans for different plotting options: ["shape"] = plots generated 2D shape, ["uv_rot"] = plots the sampled u-v plane due to the rotation of the earth (avg. wavelength), ["uv_wlSynth"] = plots the sampled u-v plane due wavelength synthesis (avg. Hr angle)
+    ### plotting_knobs: DICTIONARY of booleans for different plotting options: ["shape"] = plots generated 2D shape, ["uv_rot"] = plots the sampled u-v plane due to the rotation of the earth (avg. wavelength), ["uv_wlSynth"] = plots the sampled u-v plane due wavelength synthesis (avg. Hr angle)
     # ["u_vec_mag"]: plot the magnitude of he u,v vectors as a function of wavelength for each baseline, ["visibilities"]: plot visibilities as functions of u * characteristic_scale and wavelength for all baselines, ["phases"]: plot phases as functions of u * characteristic_scale and wavelength for all baselines
+    # ["vis_avgLambda"]: plot visibilities of each baseline for the average wavelength over the entire visibility curve, ["closure_phases"]: plot closure phases as a function of wavelength for each combination of 3 telescopes, ["baselines"]: plot baselines projected on the u-v plane
+
+    #### Generating list of baselines and their clockwise angles with respect to the u-v plane ####
+    Baselines = []
+    for i in range(len(Telescopes_coords)):
+        for j in range(i + 1, len(Telescopes_coords)):
+            Baseline_ang = angle_between(Telescopes_coords[i], (0., 0.)) + angle_between(Telescopes_coords[j], Telescopes_coords[i])
+            Baseline_length = np.sqrt((Telescopes_coords[i][0] - Telescopes_coords[j][0]) ** 2 + (Telescopes_coords[i][1] - Telescopes_coords[j][1]) ** 2)
+            Baselines.append((Baseline_length, Baseline_ang))
+
+    closurePhase_combs = np.array(list(combinations(range(1, len(Telescopes_coords) + 1), 3))) # List of tuples containing all possible combnitions of 3 telescopes
 
     #### Defining required quantities (nomenclature according to Busch book) ####
     lat, dec, Hr_angles = source_params["lat"], source_params["dec"], source_params["Hr_angles"] # observatory lat [deg], target dec [deg], target Hour angles [deg]
     characteristic_scale, dist = source_params["characteristic_scale"], source_params["dist"] # Characteristic scale of the shape [deg], distance of the shape [pc]
-    sigma_grid = np.linspace(- 2 * characteristic_scale, 2  * characteristic_scale, 500) # Defining grid of coordinates to be observed [deg]
+    sigma_grid = np.linspace(- 5 * characteristic_scale, 5  * characteristic_scale, 500) # Defining grid of coordinates to be observed [deg]
     sigma_u, sigma_v = np.meshgrid(sigma_grid, sigma_grid) # Generating a meshgrid of the u,v plane
     u_0, v_0 = source_params["source_center_lm"][0], source_params["source_center_lm"][1] # Source center in the l,m plane [deg]
     theta_0 = np.arctan2(u_0, v_0) # Angular radial distance of the source center from the phase center (l, m = 0) (1.20)
@@ -258,14 +277,16 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
     u_2D_vec = np.array([[(B[b] / wavelengths[w]) * np.array([cos(deg2rad(B_rotation[b])), sin(deg2rad(B_rotation[b]))]) for w in range(len(wavelengths))] for b in range(len(B))]) # Matrix of u vectors for all baselines and wavelengths (Eq. 1.22)
     u_sigma_2D = np.array([[exp(-1j * 2 * pi * ((u_2D_vec[b][w][0]) * sigma_u + u_2D_vec[b][w][1] * sigma_v)) for w in range(len(wavelengths))] for b in range(len(B))]) # Generating spatial wavelength coverage of of each given Baseline at every wavelength across the u-v plane
 
-    u_2D_vec_avg = np.array([(B[b] / average(wavelengths)) * np.array([cos(deg2rad(B_rotation[b])), sin(deg2rad(B_rotation[b]))]) for b in range(len(B))]) # 1D vector of baselines at the average sampled wavelength
-    u_sigma_2D_avg = np.array([real(exp(-1j * 2 * pi * ((u_2D_vec_avg[b][0]) * sigma_u + u_2D_vec_avg[b][1] * sigma_v))) for b in range(len(B))]) # Generating spatial wavelength coverage of of each given Baseline at the average wavelength value
-
-    #### Generating visibilities ####
+    #### Generating visibilities (2D) ####
     Vu_2D = np.array([[sum(sum(object_visibility * u_sigma_2D[b][w])) for w in range(len(wavelengths))] for b in range(len(Baselines))]) # Correlated flux for all Baselines and wavelengths
-    i_x_2D = np.array([[F0 + real(real(Vu_2D[b][w]) * exp(1j * 2 * pi * s[w] * detector_coords + imag(Vu_2D[b][w]))) for w in range(len(wavelengths))] for b in range(len(Baselines))]) # Fringe pattern for every Baseline and wavelength
+    i_x_2D = np.array([[F0 + real(real(Vu_2D[b][w]) * exp(1j * 2 * pi * s[w] * detector_coords + imag(Vu_2D[b][w]))) for w in range(len(wavelengths))] for b in range(len(Baselines))]) # Fringe pattern for every Baseline and wavelengths
     visibilities_2D = np.array([[real(Vu_2D[b][w]) for w in range(len(wavelengths))] for b in range(len(Baselines))]) # Visibility for each Baseline and wavelength
-    phases_2D = np.array([[imag(Vu_2D[b][w]) for w in range(len(wavelengths))] for b in range(len(Baselines))]) # Phase for each Baseline and wavelength
+    phases_2D = np.array([[rad2deg(imag(Vu_2D[b][w])) for w in range(len(wavelengths))] for b in range(len(Baselines))]) # Phase for each Baseline and wavelength
+    closurePhases_2D = np.array([sum([phases_2D[i - 1] for i in closurePhase_combs[j]], axis = 0) for j in range(len(closurePhase_combs))])
+
+    #### Generating visibilities (1D with average wavelength) ####
+    u_megaLambda_vec = np.linspace(0, max(B) / average(wavelengths), 500)
+    Vu_megaLambda = np.array([sum(object_visibility * exp(-1j * 2 * pi * ((u_megaLambda_vec[m]) * sigma_grid))) for m in range(len(u_megaLambda_vec))])
 
     ######## PLOTTING ######## ######## PLOTTING ######## ######## PLOTTING ######## ######## PLOTTING ######## ######## PLOTTING ######## ######## PLOTTING ######## ######## PLOTTING ######## ######## PLOTTING ########
 
@@ -314,13 +335,14 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
             u_this_b = np.array([u_2D_vec[b][i][0] for i in range(len(u_2D_vec[b]))])
             v_this_b = np.array([u_2D_vec[b][i][1] for i in range(len(u_2D_vec[b]))])
 
-            phase_this_b = np.array([visibilities_2D[b][i] for i in range(len(visibilities_2D[b]))])
+            vis_this_b = np.array([visibilities_2D[b][i] for i in range(len(visibilities_2D[b]))])
             g = g + 1
 
             plt.subplot(len(u_2D_vec), 2, g)
-            plt.plot(wavelengths / 1e-6, phase_this_b, color ='goldenrod')
+            plt.plot(wavelengths / 1e-6, vis_this_b, color ='goldenrod', label = "B = {0} [m], $\\theta$ = {1}$^o$".format(B[b], B_rotation[b]))
             plt.ylabel("Visibility", fontsize = fontsize - 2)
             plt.tick_params(labelsize = fontsize - 2)
+            plt.legend()
 
             if b == len(u_2D_vec) - 1:
                 plt.xlabel("Wavelength [$\mu$m]", fontsize=fontsize - 2)
@@ -328,7 +350,7 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
             g = g + 1
 
             plt.subplot(len(u_2D_vec), 2, g)
-            plt.plot(sqrt(u_this_b ** 2 + v_this_b ** 2) * characteristic_scale, phase_this_b, color ='skyblue')
+            plt.plot(sqrt(u_this_b ** 2 + v_this_b ** 2) * characteristic_scale, vis_this_b, color ='skyblue')
             # plt.plot(u_this_b * characteristic_scale, vis_this_b, color = 'skyblue', label = 'u')
             # plt.plot(v_this_b * characteristic_scale, vis_this_b, color = 'mediumorchid', label = 'v')
             plt.tick_params(labelsize = fontsize - 2)
@@ -338,6 +360,30 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
 
             if b == len(u_2D_vec) - 1:
                 plt.xlabel("$u\Delta\\sigma$", fontsize=fontsize - 2)
+        plt.show()
+
+    if plotting_knobs["vis_avgLambda"] == True:
+
+        plt.plot(u_megaLambda_vec * 1e-6, real(Vu_megaLambda), color = 'blueviolet')
+        for b in range(len(Baselines)):
+
+            vis_this_b = np.array([visibilities_2D[b][i] for i in range(len(visibilities_2D[b]))])
+            plt.plot(B[b] / (average(wavelengths) * 1e6), vis_this_b[int(len(vis_this_b) / 2)], 'o', markerfacecolor = 'lawngreen', markeredgecolor = 'white')
+
+        plt.title("$\lambda$ = {0} [$\mu$m]".format(round(average(wavelengths) * 1e6, 1)))
+        plt.xlabel("[$M\lambda$]", fontsize=fontsize)
+        plt.ylabel("Visibility", fontsize=fontsize)
+        plt.show()
+
+    if plotting_knobs["baselines"] == True:
+        for i in range(len(Telescopes_coords)):
+            for j in range(i + 1, len(Telescopes_coords)):
+                plt.plot(np.array([Telescopes_coords[i][0], Telescopes_coords[j][0]]), np.array([Telescopes_coords[i][1], Telescopes_coords[j][1]]), 'o-', label = 'T {0}, {1}'.format(i + 1, j + 1))
+        plt.title("Baselines")
+        plt.xlabel("u [m]", fontsize = fontsize)
+        plt.ylabel("v [m]", fontsize = fontsize)
+        plt.tick_params(labelsize = fontsize)
+        plt.legend()
         plt.show()
 
     if plotting_knobs["phases"] == True:
@@ -353,9 +399,10 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
             g = g + 1
 
             plt.subplot(len(u_2D_vec), 2, g)
-            plt.plot(wavelengths / 1e-6, phase_this_b, color ='olivedrab')
+            plt.plot(wavelengths / 1e-6, phase_this_b, color ='olivedrab', label = "B = {0} [m], $\\theta$ = {1}$^o$".format(round(B[b], 1), round(B_rotation[b], 1)))
             plt.ylabel("Phase [$^o$]", fontsize = fontsize - 2)
             plt.tick_params(labelsize = fontsize - 2)
+            plt.legend()
 
             if b == len(u_2D_vec) - 1:
                 plt.xlabel("Wavelength [$\mu$m]", fontsize=fontsize - 2)
@@ -364,6 +411,7 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
 
             plt.subplot(len(u_2D_vec), 2, g)
             plt.plot(sqrt(u_this_b ** 2 + v_this_b ** 2) * characteristic_scale, phase_this_b, color ='slateblue')
+            plt.plot(sqrt(u_this_b[int(len(u_this_b) / 2)] ** 2 + v_this_b[int(len(v_this_b) / 2)] ** 2) * characteristic_scale, phase_this_b[int(len(phase_this_b) / 2)], 'o', color = 'green')
             # plt.plot(u_this_b * characteristic_scale, vis_this_b, color = 'skyblue', label = 'u')
             # plt.plot(v_this_b * characteristic_scale, vis_this_b, color = 'mediumorchid', label = 'v')
             plt.tick_params(labelsize = fontsize - 2)
@@ -375,23 +423,37 @@ def run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_k
                 plt.xlabel("$u\Delta\\sigma$", fontsize=fontsize - 2)
         plt.show()
 
+    if plotting_knobs["closure_phases"] == True:
+
+        for c in range(len(closurePhase_combs)):
+            if c == 0:
+                plt.plot(wavelengths / 1e-6, closurePhases_2D[c], label = 'Telescopes {0}'.format(closurePhase_combs[c]))
+            else:
+                plt.plot(wavelengths / 1e-6, closurePhases_2D[c], label = '{0}'.format(closurePhase_combs[c]))
+        plt.xlabel("Wavelength [$\mu$m]", fontsize=fontsize - 2)
+        plt.ylabel("Closure phase [$^o$]", fontsize = fontsize - 2)
+        plt.tick_params(labelsize = fontsize - 2)
+        plt.legend()
+        plt.show()
+
+
 ######## EXAMPLE ######## ######## EXAMPLE ######## ######## EXAMPLE ######## ######## EXAMPLE ########
 
-source_center_lm = [0. * mas2deg, 0. * mas2deg] # [deg]
-Baselines = [(100, 45.), (23, 12.), (33, 82.)] # M, deg
+source_center_lm = [0.1 * mas2deg, 0.1 * mas2deg] # [deg]
+Telescopes_coords = [(22., 0.), (-2., 3.), (43., 9.), (-8., -3.)] # m, m
 characteristic_scale, dist = 1 * mas2deg, 400. # deg, pc
 shape = "gaussian_disk"
 shape_params = [1., 0., characteristic_scale / 2.] # look at shape generation functions - the input here has to be all the parameters EXCEPT sigma_grid, x0 and y0
-wavelengths = np.linspace(5, 130, 1000) * 1e-6 # [m]
+wavelengths = np.linspace(3, 13, 500) * 1e-6 # [m]
 Hr_angle, dec, lat = 28.2576335631872, -21.95482, -24.6273 # [deg]
 Hr_angle_vec = np.linspace(Hr_angle - 50., Hr_angle + 16., 50) # [deg]
-beta = 50. * as2deg # Beam angle of incidence on the detector [deg]
-detector_coords = np.linspace(-0.008, 0.008, 1000) # [m]
+beta = 1000. * as2deg # Beam angle of incidence on the detector [deg]
+detector_coords = np.linspace(-0.1, 0.1, 1000) # [m]
 source_params = {"source_center_lm": source_center_lm, "characteristic_scale": characteristic_scale, "dist": dist, "shape": shape, "shape_params": shape_params, "lat": lat, "dec": dec, "Hr_angles": Hr_angle_vec}
-plotting_knobs = {"shape": True, "uv_rot": True, "uv_wlSynth": True, "u_vec_mag": True, "visibilities": True, "phases": True}
+plotting_knobs = {"shape": True, "uv_rot": False, "uv_wlSynth": False, "u_vec_mag": False, "visibilities": False, "phases": False, "vis_avgLambda": True, "closure_phases": True, "baselines": True}
 
 #### PLOTTING KNOBS ####
-run(wavelengths, beta, detector_coords, Baselines, source_params, plotting_knobs)
+run(wavelengths, beta, detector_coords, Telescopes_coords, source_params, plotting_knobs)
 
 
 
